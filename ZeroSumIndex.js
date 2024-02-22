6b7d32e871010d064a7aae454326bd44e46d0979
saquib <mohd.saquib@faircent.com>
2024-02-22 16:43:24 +0530

    zero sum index

--- ZeroSumIndex.js
+++ ZeroSumIndex.js
@@ -1,1 +1,21 @@
 //Zero Sum Index
+
+function zeroSumIndex(arr){
+    if(arr.length == 0){
+        return [-1,-1];
+    }
+    let l = 0;
+    let r = 0;
+    while (l < r){
+        let sum = arr[l] + arr[r]
+        if(sum == 0){
+            return [l,r];
+        }else if(sum < 0){
+            l++;
+        }else{
+            r--;
+        }
+    }
+}
+
+console.log([-3,-2,1,0-2,-4]);
